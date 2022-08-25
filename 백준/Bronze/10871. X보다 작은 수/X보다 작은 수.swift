2f7f35bdbffd3143630b6d9456a497d820313365
devjoonn <82807263+devjoonn@users.noreply.github.com>
2022-08-25 14:50:18 +0900

    주석 추가

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.swift
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.swift
@@ -9,3 +9,5 @@ for i in secondLine {
         print(i)
     }
 }
+
+// map(_:)과 Int의 이니셜라이저를 통해 Int 타입으로 변환할 때 String.SubSequence -> Int 보다 String.SubSequence -> String -> Int의 수행속도가 더 빠르다는 것을 알게 됨.
