71a6da0554e584092f3921f097b81d760cbb2ce6
devjoonn <82807263+devjoonn@users.noreply.github.com>
2022-09-18 23:26:49 +0900

    [Bronze II] Title: 벌집, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+let num = Int(readLine()!)!
+var sum = 1
+var count = 1
+
+for i in 1...num {
+    if num == 1 {
+        print(count)
+        break
+    }
+    if num <= sum {
+        print(count)
+        break
+    }
+    
+    sum += 6*i
+    count += 1
+}
