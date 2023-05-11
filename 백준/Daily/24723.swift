f744cb59ca9b97f1d8fe88ffb42700d62b57ed5d
Hyeonjoonn <phj3046@gmail.com>
2023-05-11 16:57:06 +0900

    BOJ 24723

---
+++ 백준/Daily/24723.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  algolithm
+//
+//  Created by 박현준 on 2023/05/11.
+//MARK: - 24723
+
+// 1 -> 2
+// 2 -> 4
+// 3 -> 8
+// 4 -> 16
+// 5 -> 32
+
+import Foundation
+
+let N = Int(readLine()!)!
+
+let result = pow(2, N)
+
+print(result)
+
