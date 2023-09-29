a645c99debdbcb53a2d1b58822575904a35da9c7
Hyeonjoonn <phj3046@gmail.com>
2023-09-29 15:08:27 +0900

    BOJ 11726

---
+++ 백준/Python/Daily/11726.py
@@ -0,0 +1,12 @@
+#11726
+import sys
+
+n = int(sys.stdin.readline())
+dp = list(0 for i in range(1001)) 
+
+dp[1] = 1
+dp[2] = 2
+for i in range(3, 1001):
+  dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n] % 10007)
